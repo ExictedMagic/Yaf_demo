170250480dc2207903257c184e1d2f962848d5b0
pmsun <411977255@qq.com>
2013-12-18 00:20:07 +0800

    修改分页类

--- library/Page.php
+++ library/Page.php
@@ -4,7 +4,15 @@
  * Date: 13-12-16
  * Time: 下午11:33
  * Desc: 分页类
- * Usage: $page = new Page(100, array('a' => 1, 'b' => '2')); $page->create($current_page);
+ * Usage:
+ *  $page_template = array(
+ *  'blur'  => '<a href="%url%">%page%</a>',//非当前页样式
+ *  'focus' => '<span>%page%</span>',//当前页样式
+ *  'prev'  => '<a href="%url%">上一页</a>',//上一页样式
+ *  'next'  => '<a href="%url%">下一页</a>',//下一页样式
+ *  );
+ *  $page = new Page(3, $page_template, array("a" => "1", "b" => 2));
+ *  echo $page->create($this->_page);
  */
 class Page {
     //当前页码
@@ -19,21 +27,24 @@ class Page {
     protected $page_size;
     //用于构造地址的参数，例如一些查询条件
     protected $url_array;
+    //分页的html模版
+    protected $template;
 
-    public function __construct($total, $url_array = array(), $page_size = 5, $per_page = 10) {
+    public function __construct($total, $template = array(), $url_array = array(), $page_size = 5, $per_page = 1) {
         $this->page = 1;
         $this->total = $total;
         $this->page_size = $page_size;
         $this->per_page = $per_page;
         $this->total_page = intval(ceil($total / $per_page));
         $this->url_array = $url_array;
+        $this->template = $template;
     }
 
     public function create($page) {
         $this->page = $page;
         $page_half = intval($this->page_size / 2);
-        $page_start = max(1, $this->page - $page_half);
-        $page_end = min($page_start + $this->page_size - 1, $this->total_page);
+        $page_start = $this->page <= $this->total_page ? max(1, $this->page - $page_half) : 1;
+        $page_end =min($page_start + $this->page_size - 1, $this->total_page);
         return $this->pageHtml($page_start, $page_end);
     }
 
@@ -41,14 +52,22 @@ class Page {
         $page_array = range($page_start, $paga_end);
         $page_html = "";
         $url_str = empty($this->url_array) ? "?page=" : "?" . http_build_query($this->url_array) . "&page=";
-        if (!empty($page_array)) {
+        if (!empty($page_array) && $this->total_page > 1) {
+            $this->page = $this->total_page >= $this->page ? $this->page : 1;
+            //上一页
+            $prev_url = $url_str . ($this->page - 1);
+            $page_html .= $this->page > 1 && isset($this->template['prev']) ? str_replace("%url%", $prev_url, $this->template['prev']) : "";
             foreach ($page_array as $p) {
                 if ($this->page == $p) {
-                    $page_html .= "<span style='color:red;'>{$p}</span>";
+                    $page_html .= isset($this->template['focus']) ? str_replace("%page%", $p, $this->template['focus']) : "";
                 } else {
-                    $page_html .= "<a href='{$url_str}{$p}'>{$p}</a>";
+                    $url = $url_str . $p;
+                    $page_html .= isset($this->template['blur']) ? str_replace(array("%url%", "%page%"), array($url, $p), $this->template['blur']) : "";
                 }
             }
+            //下一页
+            $next_url = $url_str . ($this->page + 1);
+            $page_html .= $this->page < $this->total_page && isset($this->template['next']) ? str_replace("%url%", $next_url, $this->template['next']) : "";
         }
         return $page_html;
     }
